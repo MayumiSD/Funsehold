e35b2ce20c4352f37b6a17ffc867874f1dcf4dc9
MayumiSD <sun_nsmile@yahoo.co.jp>
2020-07-12 21:59:01 +0900

    file add

---
+++ logout.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+if (isset($_SESSION["NAME"])) {
+    $errorMessage = "ログアウトしました。";
+} else {
+    $errorMessage = "セッションがタイムアウトしました。";
+}
+
+// セッションの変数のクリア
+$_SESSION = array();
+
+// セッションクリア
+session_destroy();
+
+echo htmlspecialchars($errorMessage, ENT_QUOTES);
+
+require './vendor/autoload.php';
+$smarty = new Smarty();
+$smarty->display('index.tpl');
+
+
+?>
+
+
