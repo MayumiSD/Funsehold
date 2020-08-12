985d37213c4cf23f0f75fe69e9962c80650b879c
MayumiSD <sun_nsmile@yahoo.co.jp>
2020-08-12 16:27:23 +0900

    　fetch

--- receipe.php
+++ receipe.php
@@ -3,8 +3,8 @@ session_start();
 
 require './vendor/autoload.php';
 $suggestionReceipe = getReceipeName();
-foreach ($suggestionReceipe as $value)
-echo $value;
+foreach ($suggestionReceipe as $value){
+echo $value;}
 
 $smarty = new Smarty();
 $smarty->display('receipe.tpl');
