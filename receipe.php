99a4c01c3f16c26a1f9157112472f3c1a020df6d
MayumiSD <sun_nsmile@yahoo.co.jp>
2020-08-12 15:42:30 +0900

    　fetch

--- receipe.php
+++ receipe.php
@@ -15,9 +15,9 @@ function getReceipeName (){
             // 接続先DBリンク
             $connect = "mysql:host={$dsn['host']};dbname={$dsn['dbnm']}";
             $pdo = new PDO($connect, $dsn['user'], $dsn['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
-            $stmt = $pdo->prepare('SELECT * FROM receipe ');
-            $stmt->execute();
-            $row = $stmt->fetch(PDO::FETCH_NUM);
+            $sql = 'SELECT * FROM receipe ';
+            $stmt=$pdo->query($sql);
+            $row = $stmt->fetchAll(PDO::FETCH_COLUMN);
 
             $suggestionReceipe = $row['receipe_name'];
             return $suggestionReceipe;
