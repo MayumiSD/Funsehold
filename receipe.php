2f43d7ade38e6b6679137e2523cd305919506adb
MayumiSD <sun_nsmile@yahoo.co.jp>
2020-08-16 14:56:31 +0900

    　smarty 出力

--- receipe.php
+++ receipe.php
@@ -13,7 +13,7 @@ function getReceipeName (){
             require_once'DSN.php';
             // 接続先DBリンク
             $connect = "mysql:host={$dsn['host']};dbname={$dsn['dbnm']}";
-            $pdo = new PDO($connect, $dsn['user'], $dsn['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+            $pdo = new PDO($connect, $dsn['user'], $dsn['pass'], [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
             $stmt = $pdo->prepare('SELECT * FROM receipe ');
             $stmt->execute();
 
