86e24bd143c6611b9e4d43d6f681f5a8534a1704
MayumiSD <sun_nsmile@yahoo.co.jp>
2020-08-20 21:46:23 +0900

    　cook.phめ

--- cook.php
+++ cook.php
@@ -1,10 +1,22 @@
 <?php
 session_start();
 
-echo $_GET["receipeid"]; 
+$receipeInfo = getReceipeInfo($_GET["receipeid"]);
+var_dump($receipeInfo);
 
 
 require './vendor/autoload.php';
 $smarty = new Smarty();
 
 $smarty->display('cook.tpl');
+
+
+function getReceipeInfo($receipeId){
+    require_once'DSN.php';
+    // 接続先DBリンク
+    $connect = "mysql:host={$dsn['host']};dbname={$dsn['dbnm']}";
+    $pdo = new PDO($connect, $dsn['user'], $dsn['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+    $stmt = $pdo->prepare('SELECT * FROM receipe where receipe_id = ?');
+    $stmt->execute([$receipeId]);
+    return $stmt->fetch(PDO::FETCH_ASSOC); 
+}
