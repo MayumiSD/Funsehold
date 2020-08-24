c92bb15f3b3fba9eea421f9880984e0a30d45592
MayumiSD <sun_nsmile@yahoo.co.jp>
2020-08-24 15:33:22 +0900

    　favoriteReceipe v2

--- receipe.php
+++ receipe.php
@@ -26,12 +26,9 @@ function getReceipeName (){
 function getFavoriteInfo($email){
     if (isset($_SESSION["email"])){
         require_once'DSN.php';
-        // 接続先DBリンク
-        $connect = "mysql:host={$dsn['host']};dbname={$dsn['dbnm']}";
-        $pdo = new PDO($connect, $dsn['user'], $dsn['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
-        $stmt = $pdo->prepare('SELECT * FROM favorite_receipe where email = ?');
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $sql = $pdo->prepare('SELECT * FROM favorite_receipe where email = ?');
+        $sql->execute();
+        return $sql->fetchAll(PDO::FETCH_ASSOC);
 
     }
 
