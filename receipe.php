70e39bf1d1c193761520ef1e2ea177df1b7115e5
MayumiSD <sun_nsmile@yahoo.co.jp>
2020-08-24 16:53:20 +0900

    　favoriteReceipe v4

--- receipe.php
+++ receipe.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 $receipeName = getReceipeName();
+require_once'DSN.php';
+// 接続先DBリンク
+$connect = "mysql:host={$dsn['host']};dbname={$dsn['dbnm']}";
+$pdo = new PDO($connect, $dsn['user'], $dsn['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 
 echo $_SESSION["email"];
 
@@ -25,10 +29,8 @@ function getReceipeName (){
 
 if (isset($_SESSION["email"])){
         $userid = $_SESSION["email"];
-        require_once'DSN.php';
-        // 接続先DBリンク
-        $connect = "mysql:host={$dsn['host']};dbname={$dsn['dbnm']}";
-        $pdo = new PDO($connect, $dsn['user'], $dsn['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        $connect;
+        $pdo;
         $sql = $pdo->prepare('SELECT * FROM favorite_receipe where email = ?');
         $sql->execute($userid);
         $data = $sql->fetchAll(PDO::FETCH_ASSOC);
